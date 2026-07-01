f530286c8a2996f7011ce6db69d4aeb8500d1643
emenoghaebube21-eng <emenoghaebube21@gmail.com>
2026-06-30 21:37:52 -0700

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -158,3 +158,17 @@ if(year){
 year.textContent=new Date().getFullYear();
 
 }v
+
+window.addEventListener("load",()=>{
+
+const loader=document.getElementById("loader");
+
+loader.style.opacity="0";
+
+setTimeout(()=>{
+
+loader.style.display="none";
+
+},600);
+
+});
