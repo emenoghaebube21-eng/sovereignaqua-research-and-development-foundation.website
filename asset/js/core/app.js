c3f5b9a71841930797a25cbc6059dfde71fd65b7
emenoghaebube21-eng <emenoghaebube21@gmail.com>
2026-07-19 18:54:24 -0700

    Initialize navigation and hero modules on DOMContentLoaded

---
+++ asset/js/core/app.js
@@ -0,0 +1,7 @@
+import { initNavigation } from "../modules/navigation.js";
+import { initHero } from "../modules/hero.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initNavigation();
+  initHero();
+});
